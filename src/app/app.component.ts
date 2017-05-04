eb2fbf4ae6cf69f63b09b55bc1a4022877bc4752
abbes_yassine <abbesyassine4@gmail.com>
2017-05-04 21:04:02 +0100

    Fixing ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-
+<header-component></header-component>
 <router-outlet></router-outlet>
+<footer-component></footer-component>
 `
 })
 export class AppComponent {
